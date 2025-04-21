2402b154a9aa29357694922eb5e1e5a2829b9ca7
Andrew <106377306+salopower@users.noreply.github.com>
2025-04-21 11:09:15 -0400

    feat(reporting): integrate Mochawesome, Allure and BrowserStack (#19)
    * feat(reporting): integrate Mochawesome, Allure and BrowserStack
    
    * fix: remove reports from PR, updated .gitignore

--- home_works/home_work_17/webdriver-io/tests/amazon.spec.ts
+++ home_works/home_work_17/webdriver-io/tests/amazon.spec.ts
@@ -3,20 +3,16 @@ import AmazonPage from '../src/pages/amazon.page';
 describe('Amazon Tests', () => {
     const amazonPage = new AmazonPage();
 
-    beforeEach(async () => {
-        await amazonPage.open();
-    });
-
     describe('Logo Verification', () => {
         it('should verify logo is visible and clickable', async () => {
             await amazonPage.verifyLogo();
         });
     });
 
-    describe('Product Search', () => {
-        it('should find products with "Charger" in title', async () => {
-            await amazonPage.searchForProduct('phone charger');
-            await amazonPage.verifySearchResults();
-        });
-    });
+    // describe('Product Search', () => {
+    //     it('should find products with "Charger" in title', async () => {
+    //         await amazonPage.searchForProduct('phone charger');
+    //         await amazonPage.verifySearchResults();
+    //     });
+    // });
 });
